0d8387a481c7ae40ea584497bbd59a866ed5f7bb
vstrom650xt <cerveramonzo@gmail.com>
2023-02-07 21:18:25 +0100

    medio lista generiaca

--- src/main/java/org/example/listas/generics/LinkedList.java
+++ src/main/java/org/example/listas/generics/LinkedList.java
@@ -2,30 +2,38 @@ package org.example.listas.generics;
 
 import java.lang.reflect.Array;
 
-public class LinkedList <T>{
+public class LinkedList <T> {
     private Node<T> head;
     private Node<T> tail;
     private int size;
 
-    public LinkedList (){
+    public LinkedList() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
 
-    public int size() { return size;}
-    public Node<T> getHead() { return head;}
+    public int size() {
+        return size;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
     public Node<T> getTail() {
         return tail;
     }
 
-    public boolean isEmpty() { return size==0;}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-    public void addHead(T elem){
+    public void addHead(T elem) {
         Node<T> node = new Node<>(elem);
-        if(isEmpty()){
-            head=node;
-            tail=node;
+        if (isEmpty()) {
+            head = node;
+            tail = node;
         } else {
             node.setNext(head);
             head = node;
@@ -33,11 +41,11 @@ public class LinkedList <T>{
         size++;
     }
 
-    public void addTail(T elem){
+    public void addTail(T elem) {
         Node<T> node = new Node<>(elem);
-        if(isEmpty()){
-            head=node;
-            tail=node;
+        if (isEmpty()) {
+            head = node;
+            tail = node;
         } else {
             tail.setNext(node);
             tail = node;
@@ -45,60 +53,60 @@ public class LinkedList <T>{
         size++;
     }
 
-    public int search(T elem){
+    public int search(T elem) {
         int result = -1;
-        if(isEmpty())
+        if (isEmpty())
             return result;
         Node<T> node = head;
         for (int i = 0; i < size; i++) {
-            if(node.getElem().equals(elem))
+            if (node.getElem().equals(elem))
                 return i;
             node = node.getNext();
         }
         return result;
     }
 
-    public T[] toArray(Class c){
-        T[] vector = (T[]) Array.newInstance(c,size);
+    public T[] toArray(Class c) {
+        T[] vector = (T[]) Array.newInstance(c, size);
         Node node = head;
         for (int i = 0; i < size; i++) {
-            vector[i] =(T) node.getElem();
+            vector[i] = (T) node.getElem();
             node = node.getNext();
         }
         return vector;
     }
 
-    public T remove(int index){
+    public T remove(int index) {
         T result = null;
-        if(isEmpty() || index < 0 || index>=size)
+        if (isEmpty() || index < 0 || index >= size)
             return result;
-        boolean tailChange = (index == size-1);
-        if(index==0){
+        boolean tailChange = (index == size - 1);
+        if (index == 0) {
             result = head.getElem();
             head = head.getNext();
         } else {
             Node<T> aux1 = head;
             Node<T> aux2 = head.getNext();
-            while(index>1){
+            while (index > 1) {
                 aux1 = aux2;
                 aux2 = aux2.getNext();
                 index--;
             }
             result = aux2.getElem();
             aux1.setNext(aux2.getNext());
-            if(tailChange)
+            if (tailChange)
                 tail = aux1;
         }
         size--;
         return result;
     }
 
-    public T get(int index){
+    public T get(int index) {
         T result = null;
-        if (isEmpty() || index <0 || index>=size)
+        if (isEmpty() || index < 0 || index >= size)
             return result;
         Node<T> node = head;
-        while(index>0){
+        while (index > 0) {
             node = node.getNext();
             index--;
         }
@@ -109,13 +117,13 @@ public class LinkedList <T>{
         return result;
     }
 
-    public void clear(){
+    public void clear() {
         head = null;
         tail = null;
         size = 0;
     }
 
-    public boolean addAll(LinkedList<T> list){
+    public boolean addAll(LinkedList<T> list) {
         boolean updated = false;
         Node<T> node = list.getHead();
 
@@ -128,19 +136,19 @@ public class LinkedList <T>{
     }
 
     @Override
-    public boolean equals(Object obj){
-        if(!(obj instanceof LinkedList))
+    public boolean equals(Object obj) {
+        if (!(obj instanceof LinkedList))
             return false;
         LinkedList<T> list = (LinkedList) obj;
 
-        if(list.size() != this.size)
+        if (list.size() != this.size)
             return false;
 
         Node<T> aux1 = this.head;
         Node<T> aux2 = list.getHead();
 
-        while(aux1 != null){
-            if(!aux1.equals(aux2))
+        while (aux1 != null) {
+            if (!aux1.equals(aux2))
                 return false;
             aux1 = aux1.getNext();
             aux2 = aux2.getNext();
@@ -149,16 +157,17 @@ public class LinkedList <T>{
     }
 
     @Override
-    public String toString(){
-        return "{ size: " + size + ", Element: " + ((head==null) ? "}" : head.toString());
+    public String toString() {
+        return "{ size: " + size + ", Element: " + ((head == null) ? "}" : head.toString());
     }
 
-    public boolean contains(T elem){
+    public boolean contains(T elem) {
         Node<T> node = head;
         for (int i = 0; i < size; i++) {
-            if(elem.equals(node.getElem()))
+            if (elem.equals(node.getElem()))
                 return true;
             node = node.getNext();
         }
         return false;
     }
+}
